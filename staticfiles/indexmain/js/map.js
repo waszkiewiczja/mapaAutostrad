8b1ff028692c19f789b934d663f04b5edb4b2e9d
Jed <blady209@gmail.com>
2022-01-20 01:30:45 +0100

    all

--- staticfiles/indexmain/js/map.js
+++ staticfiles/indexmain/js/map.js
@@ -19,7 +19,7 @@ const ilosc_inwestycji = Number(
 const buildList = () => {
   const modal = document.querySelector(".modal");
 
-  const url = `https://mapaauto.herokuapp.com/api/road-detail/${activeRoad}/`;
+  const url = `http://www.mapaautostrad.pl/api/road-detail/${activeRoad}/`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
